2cf7dbb6d278f9eee2a4ea2a755973265019dfba
Benjamin Lim <benjaminlim@Benjamins-MacBook-Air.local>
2022-05-26 01:37:48 +0800

    answers

--- test/game4Test.js
+++ test/game4Test.js
@@ -7,8 +7,14 @@ describe("Game4", function() {
     await game.deployed();
 
     // nested mappings are rough :}
+    const signer1 = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+    const address1 = await signer1.getAddress();
+    const address2 = await signer2.getAddress();
 
-    await game.win();
+    await game.connect(signer1).write(address2);
+
+    await game.connect(signer2).win(address1);
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
--- test/game5Test.js
+++ test/game5Test.js
@@ -7,8 +7,15 @@ describe("Game5", function() {
     await game.deployed();
 
     // good luck
-
-    await game.win();
+    const allAcc = await ethers.provider.listAccounts();
+    let x;
+    for (let i=0; i<allAcc.length; i++) {
+      if (Number(allAcc[i]) > Number("0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf")) {
+        return x = i;
+      }
+    }
+    const signer = ethers.provider.getSigner(x);
+    await game.connect(signer).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
